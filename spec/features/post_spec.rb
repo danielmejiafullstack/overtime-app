8ac50673f0b1198c68ef3aa19f2af2b1898fa046
Daniel Mejia <dmejia@fullstacklabs.co>
2020-09-12 13:58:55 -0500

    Refactor implementation of creations process for post

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -2,25 +2,29 @@ require 'rails_helper'
 
 describe 'navigate' do
   describe 'index' do
-    it 'can be reached successfully' do
+    before do
       visit posts_path
+    end
+
+    it 'can be reached successfully' do
       expect(page.status_code).to eq(200)
     end
 
     it 'has a title of Posts' do
-      visit posts_path
       expect(page).to have_content(/Posts/)
     end
   end
 
   describe 'creation' do
-    it 'has a new form that can be reached successfully' do
+    before do
       visit new_post_path
+    end
+
+    it 'has a new form that can be reached successfully' do
       expect(page.status_code).to eq(200)
     end
 
     it 'can be create from a new form page successfully' do
-      visit new_post_path
       fill_in 'post[date]', with: Date.today
       fill_in 'post[rationale]', with: 'Some rationale'
       click_on 'Save'
